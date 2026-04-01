104bfc8ae7dd91d68e4cb9cae3c7818f7a278294
Josh-Hiz <jhizgiaev2704@gmail.com>
2026-04-01 03:23:41 -0400

    Initial commit

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 A complex parallelized 3D fractal renderer and engine implemented in C++ and CUDA.
 
 Created by Joshua Hizgiaev and Marcos Traverso
+
+## Building
+
+Run the following to install GLFW3, OpenGL with Mesa (open-source implementation for Linux), the math library for OpenGL.
+
+```sh
+sudo apt instal libgl1-mesa-dev libglu1-mesa-dev libglfw3-dev libglm-dev mesa-utils
+```
+
+These libraries will be statically linked within CMake besides GLM, which is just headers.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, from FractalGPU!\n";
+int main() {
+    GLFWwindow *window;
+
+    if (!glfwInit()) {
+        std::cout << "GLFW is not init\n";
+        return -1;
+    }
+    window = glfwCreateWindow(1024, 720, "FractalGPU", NULL, NULL);
+    if (!window) {
+        std::cout << "Window creation failed\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0);
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
     return 0;
 }
